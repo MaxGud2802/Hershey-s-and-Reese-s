c5d0463e07503016672cf39fd5c173afa7d5c948
Maximus Gudino <m_gudino@swbell.net>
2021-12-21 16:09:31 -0600

    everything is set properly.

--- Hershey's and Reese's/ViewController.swift
+++ Hershey's and Reese's/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewBackground: UIView!
+    @IBOutlet weak var viewHolder: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    let hershesMessage = "You got peanut butter in my chocolate!"
+    let reesesMessage = "You got chocolate in my peanut butter!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
+        viewHolder.backgroundColor = .clear
+        viewBackground.backgroundColor = .yellow
     }
-
-
+    @IBAction func buttonHey(_ sender: Any) {
+        if messageLabel.text == reesesMessage {
+            messageLabel.text = hershesMessage
+            messageLabel.textAlignment = .left
+            messageLabel.textColor = .brown
+        } else if messageLabel.text == hershesMessage {
+            messageLabel.text = reesesMessage
+            messageLabel.textAlignment = .right
+            messageLabel.textColor = .orange
+        } else {
+            messageLabel.text = reesesMessage
+            messageLabel.textAlignment = .right
+            messageLabel.textColor = .orange
+        }
+    }
 }
-
