75d85df3eb0001025b90ef5f89de6c41b49075d5
Maximus Gudino <m_gudino@swbell.net>
2021-12-21 14:47:25 -0600

    Initial Commit

---
+++ Hershey's and Reese'sUITests/Hershey_s_and_Reese_sUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hershey_s_and_Reese_sUITestsLaunchTests.swift
+//  Hershey's and Reese'sUITests
+//
+//  Created by Maximus Gudino on 12/21/21.
+//
+
+import XCTest
+
+class Hershey_s_and_Reese_sUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
